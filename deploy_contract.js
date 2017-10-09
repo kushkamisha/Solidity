415e0d419a2e4e322d14677b21258b6896425659
Kuska Misha <kushkamisha@yahoo.com>
2017-10-09 16:27:50 +0300

    deploy & load contract from .sol file

--- deploy_contract.js
+++ deploy_contract.js
@@ -1,8 +1,10 @@
-var Web3 = require('web3');
+const Web3 = require('web3');
+const fs = require('fs');
+const solc = require('solc');
+
 var web3 = new Web3("https://ropsten.infura.io/m2127IH3i4kuHoojavXS");
 
 var address = "0xB305F6450158cb61277E57d80925Fcca1e3AF8e2"; // in Ropsten
-// contract address: 0xd34997f977aa146F0d8b9156d097d4504cD04Ebc
 var private_key = "0xe2dbf9358232158b14312e57944938810a771f891b761bed28a1e09544670afb";
 
 var account = web3.eth.accounts.privateKeyToAccount(private_key);
@@ -10,18 +12,34 @@ if (account.address != address) {
     exit('plz enter valid address private key');
 }
 
-console.log(account);
-
 web3.eth.accounts.wallet.add(private_key);
 web3.eth.defaultAccount = address;
 
-// Contract
+/*
+* Compile Contract and Fetch ABI
+*/ 
+var source = fs.readFileSync("../HelloWorldContract.sol", 'utf8');
+console.log('Compiling contract...');
+var compiledContract = solc.compile(source);
+
+for (var contractName in compiledContract.contracts) {
+    var bytecode = compiledContract.contracts[contractName].bytecode;
+    var abi = JSON.parse(compiledContract.contracts[contractName].interface);
+}
+
+console.log('Bytecode: ' + bytecode);
+console.log('ABI: ' + JSON.stringify(abi, undefined, 2));
+
+/*
 var bytecode = "6060604052600c600055341561001457600080fd5b60cb806100226000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680633fb5c1cb146046578063f2c9ecd814606657600080fd5b3415605057600080fd5b60646004808035906020019091905050608c565b005b3415607057600080fd5b60766096565b6040518082815260200191505060405180910390f35b8060008190555050565b600080549050905600a165627a7a72305820e920fae30ae52158b3600157608a71556f97f88b55e607016673bdc1e0e52aeb0029";
 
 var abi = [{"constant":false,"inputs":[{"name":"_number","type":"uint256"}],"name":"setNumber","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getNumber","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
-
+*/
 // Send
 var myContract = new web3.eth.Contract(abi);
+
+console.log('Deploying contract...');
+
 myContract.deploy({
     data: '0x' + bytecode,
 })
@@ -29,8 +47,7 @@ myContract.deploy({
     from: address,
     gas: 2000000, // gas limit
     // gasPrice: 1000000 // gas price in wei
-}, function(error, transactionHash){ 
-        console.log("Deploying...");
+}, function(error, transactionHash){
         if (!error) {
             console.log("Transaction hash: " + transactionHash);
         } else {
@@ -48,5 +65,5 @@ myContract.deploy({
         console.log("Confirmations: " + confirmationNumber);
     })
     .then(function(newContractInstance){
-        console.log(newContractInstance.options.address) // instance with the new contract address
+        console.log(newContractInstance.options) // instance with the new contract address
     });
--- load_contract.js
+++ load_contract.js
@@ -1,11 +1,13 @@
 // Read existing contract
 // In node http://136.243.173.186:8540
 
-var Web3 = require('web3');
+const Web3 = require('web3');
+const fs = require('fs');
+const solc = require('solc');
+
 var web3 = new Web3("https://ropsten.infura.io/m2127IH3i4kuHoojavXS");
 
 var address = "0xB305F6450158cb61277E57d80925Fcca1e3AF8e2"; // in Ropsten
-// contract address: 0xd34997f977aa146F0d8b9156d097d4504cD04Ebc
 var private_key = "0xe2dbf9358232158b14312e57944938810a771f891b761bed28a1e09544670afb";
 
 var account = web3.eth.accounts.privateKeyToAccount(private_key);
@@ -13,15 +15,26 @@ if (account.address != address) {
     console.log('plz enter valid address private key');
 }
 
-console.log(account.address);
+console.log("Account: " + account.address);
 
-var contract_address = "0x42686C872fe0031bA6BD84Aa2ACd2Ab4a1b699fA";
-var abi = [{"constant":false,"inputs":[{"name":"_number","type":"uint256"}],"name":"setNumber","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getNumber","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
+/*
+* Compile Fetch ABI
+*/ 
+var source = fs.readFileSync("../HelloWorldContract.sol", 'utf8');
+console.log('Getting ABI...');
+var compiledContract = solc.compile(source);
+
+for (var contractName in compiledContract.contracts) {
+    var abi = JSON.parse(compiledContract.contracts[contractName].interface);
+}
+var _abi = 'ABI: ' + JSON.stringify(abi, undefined, 2);
+
+var contract_address = "0x835b3b1B710c168C3A8231F4B345401eF2D2820f";
 
 var myContract = new web3.eth.Contract(abi, contract_address);
-// console.log(myContract.options.jsonInterface);
+console.log("ABI: " + myContract.options.jsonInterface);
 
-myContract.methods.getNumber().call({from: address}, function(error, result){
+myContract.methods.getWord().call({from: address}, function(error, result){
     if (!error) {
         console.log(result);
     } else {
